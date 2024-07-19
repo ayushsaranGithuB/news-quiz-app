2f81ee7c12bf1fe6b38ff6fc9edd4ae33f8df76e
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2024-07-19 13:38:05 +0530

    loading screen

--- components/LoadingScreen.jsx
+++ components/LoadingScreen.jsx
@@ -1,10 +1,12 @@
 // Universal loading screen component
+import styles from '@/styles/Loading.module.css';
 
 const LoadingScreen = ({ text = 'loading...' }) => {
     return (
-        <div className="centered card">
-            <p className="loading-screen_text">{text}</p>
-            <img src="loading.gif" alt="" />
+        <div className={"card " + styles.loading}>
+            <img src="/newspaper.svg" alt="" width={50} className={styles.icon} />
+            <p className={styles.text}>{text}</p>
+            <img src="/loading.gif" alt="" />
         </div>
     );
 };
