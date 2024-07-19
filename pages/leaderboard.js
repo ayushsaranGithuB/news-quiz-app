c8b9a8e2a6d7d1a36ab0059e2c776d79b483bebd
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2024-07-19 18:11:56 +0530

    leaderboard

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,49 +1,50 @@
 const LeaderBoard = () => {
   return (
     <>
-      <h1 className="centered">LeaderBoard</h1>
+      <div className="card">
+        <h1 className="centered">LeaderBoard</h1>
 
-      <table className="leaderboard">
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Name</th>
-            <th className="score">Score</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="rank">1</td>
-            <td className="name">John Doe</td>
-            <td className="score">100</td>
-          </tr>
-          <tr>
-            <td className="rank">2</td>
-            <td className="name">Jane Doe</td>
-            <td className="score">90</td>
-          </tr>
-          <tr>
-            <td className="rank">3</td>
-            <td className="name">John Smith</td>
-            <td className="score">80</td>
-          </tr>
-          <tr>
-            <td className="rank">4</td>
-            <td className="name">Jane Smith</td>
-            <td className="score">70</td>
-          </tr>
-          <tr>
-            <td className="rank">5</td>
-            <td className="name">John Johnson</td>
-            <td className="score">60</td>
-          </tr>
-        </tbody>
-      </table>
+        <table className="leaderboard">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th className="score">Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="rank">1</td>
+              <td className="name">John Doe</td>
+              <td className="score">100</td>
+            </tr>
+            <tr>
+              <td className="rank">2</td>
+              <td className="name">Jane Doe</td>
+              <td className="score">90</td>
+            </tr>
+            <tr>
+              <td className="rank">3</td>
+              <td className="name">John Smith</td>
+              <td className="score">80</td>
+            </tr>
+            <tr>
+              <td className="rank">4</td>
+              <td className="name">Jane Smith</td>
+              <td className="score">70</td>
+            </tr>
+            <tr>
+              <td className="rank">5</td>
+              <td className="name">John Johnson</td>
+              <td className="score">60</td>
+            </tr>
+          </tbody>
+        </table>
 
-      <div className="centered">
-        <h3>Sign-Up to see your score on the LeaderBoard</h3>
-
-        <button>Sign-Up</button>
+        <div className="centered">
+          <h4>Sign-Up to see your score on the LeaderBoard</h4>
+          <button>Sign-Up</button>
+        </div>
       </div>
     </>
   );
